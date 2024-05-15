f2d13b5b02f7c82bff47501f48e66c55dc7802d6
Andre Gradvohl <gradvohl@ft.unicamp.br>
2024-05-15 09:13:02 -0300

    Adicao de mais comentarios no codigo

--- TCP/clientTCP.c
+++ TCP/clientTCP.c
@@ -19,21 +19,28 @@
 // Funcao que implementa o chat entre cliente e servidor
 void func(int sockfd) 
 { 
+ // String para armazenar as mensagens
  char buff[MAX]; 
  int n; 
 
  // Laco infinito do chat
  for (;;) { 
+	// Apaga todo o conteudo do buffer.
 	bzero(buff, sizeof(buff)); 
 	printf("Informe uma string: "); 
 	n = 0; 
 
+	// Laco para ler caracteres do teclado e
+	// armazenar no buffer.
 	while ((buff[n++] = getchar()) != '\n') 
 		; 
+
+	buff[n]='\0';
 
         // Escreve a mensagem que esta no buffer no socket.
 	write(sockfd, buff, sizeof(buff)); 
 
+	//Apaga todo o conteudo do buffer
 	bzero(buff, sizeof(buff)); 
 
         // Le a mensagem do servidor e copia para o buffer.
@@ -50,11 +57,12 @@ void func(int sockfd)
 } 
 
 int main(int argc, char *argv[]) 
-{ 
- int sockfd, connfd; 
- struct sockaddr_in servaddr, cli; 
- /*char ipServidor[20]; 
- unsigned int tamanhoIPServidor=0;*/
+{
+ // Descritor do socket	
+ int sockfd; 
+
+ // Estruturas para armazenar os dados do servidor
+ struct sockaddr_in servaddr;
 
  if (argc < 2)
  {
@@ -62,23 +70,36 @@ int main(int argc, char *argv[])
    exit(EXIT_FAILURE);
  }
 
- // Criacao e verificacao do socket 
+ // Criacao e verificacao do socket.
+ // Parametros:
+ //   AF_INET: o Protocolo IPv4
+ //   SOCK_STREAM: Equivalente ao protocolo TCP
+ //   0: Informa um protocolo especifico na familia de protocolos. Pode deixar 0 mesmo
  sockfd = socket(AF_INET, SOCK_STREAM, 0); 
- if (sockfd == -1) { 
+ if (sockfd == -1) 
+ { 
 	printf("A criacao do socket falhou...\n"); 
 	exit(EXIT_FAILURE); 
  } 
  else
 	printf("Socket criado com sucesso ...\n"); 
 
+ // Zera todos os campos da estrutura servaddr
  bzero(&servaddr, sizeof(servaddr)); 
 
- // Atribui IP e PORT 
+ // Atribui as informacoes especificas na estrutura servaddr
+ //  A familia de protocolos e o AF_INET (IPv4)
  servaddr.sin_family = AF_INET; 
+ //  O endereco a se conectar foi passado como parametro na linha de comando (argv[1])
  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+ //  A porta a se conectar esta no PORT (nesse caso e a 4321)
  servaddr.sin_port = htons(PORT); 
 
  // Conecta o socket do client ao socket do servidor
+ // A primitiva connect recebe tres parametros:
+ //    - o identificador do socket em sockfd
+ //    - A estrutura que contem as informacoes do servidor
+ //    - O tamanho da estrutura que contem as informacoes do servidor 
  if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
 	printf("A conexão com o servidor falhou ...\n"); 
 	exit(EXIT_FAILURE); 
--- TCP/serverTCP.c
+++ TCP/serverTCP.c
@@ -20,24 +20,28 @@
 // Funcao que implementa o chat entre cliente e servidor
 void func(int sockfd) 
 { 
+  // String que armazenara as mensagens entre cliente e servidor
   char buff[MAX]; 
   int n; 
 
   // Laco infinito do chat
   for (;;) { 
-   bzero(buff, MAX); 
+   // Zera o buffer
+   bzero(buff, sizeof(buff)); 
 
-   // Le a mensagem do cliente e copia em um buffer.
+   // Le a mensagem do cliente e copia para o buffer
    read(sockfd, buff, sizeof(buff)); 
 
    // Imprime a mensagem que chegou ao buffer
    printf("Recebido do cliente: %s\t Enviar para o cliente: ", buff); 
-   bzero(buff, MAX); 
+   bzero(buff, sizeof(buff)); 
    n = 0; 
 
    // Copia a mensagem do servidor para o buffer
    while ((buff[n++] = getchar()) != '\n') 
      ; 
+
+   buff[n]='\0';
 
    // Envia a mensagem que esta no buffer para o cliente
    write(sockfd, buff, sizeof(buff)); 
@@ -52,11 +56,24 @@ void func(int sockfd)
 
 int main() 
 { 
-  int sockfd, connfd, len; 
-  struct sockaddr_in servaddr, cli; 
+  // Descritor do socket 
+  int sockfd;
+  // Descritor da conexao com o cliente
+  int connfd;
+  // Tamanho
+  int tamanho; 
+   
+  // Estrutura para armazenar os dados do servidor
+  struct sockaddr_in servaddr;
+  // Estrutura para armazenar os dados do cliente
+  struct sockaddr_in cli; 
 
-  // Criacao e verificacao do socket
-  sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+  // Criacao e verificacao do socket.
+  // Parametros:
+  //   AF_INET: o Protocolo IPv4
+  //   SOCK_STREAM: Equivalente ao protocolo TCP
+  //   0: Informa um protocolo especifico na familia de protocolos. Pode deixar 0 mesmo
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) { 
  	printf("A criacao do socket falhou ...\n"); 
 	exit(EXIT_FAILURE); 
@@ -64,14 +81,23 @@ int main()
   else
 	printf("Socket criado com sucesso ...\n"); 
 
+  // Apaga todos os dados da estrutura servaddr
   bzero(&servaddr, sizeof(servaddr)); 
 
-  // Atribui IP e PORT
+  // Atribui as informacoes especificas na estrutura servaddr
+  //  A familia de protocolos e o AF_INET (IPv4)
   servaddr.sin_family = AF_INET; 
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+  //  Endereco do qual aceita conexoes. 
+  //  Nesse caso, INADDR_ANY significa qualquer endereco.
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  //  A porta a se conectar esta no PORT (nesse caso e a 4321) 
   servaddr.sin_port = htons(PORT); 
 
   // Faz a ligacao do socket ao endereco IP e porta.
+  // A primitiva bind utiliza tres parametros:
+  //   - o descritor do socket
+  //   - a estrutura com os dados do servidor preenchida nos comandos anteriores
+  //   - o tamanho da estrutura com os dados do servidor
   if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
 	printf("A ligacao (bind) com o socket falhou...\n"); 
 	exit(EXIT_FAILURE); 
@@ -80,6 +106,9 @@ int main()
 	printf("Socket ligado (binded) com sucesso ...\n"); 
 
   // Aguardando conexões dos clientes.
+  // A primitiva listen recebe dois parametros
+  //   - o descritor do socket
+  //   - a quantidade de conexoes que podem ficar aguardando na fila
   if ((listen(sockfd, 5)) != 0) { 
 	printf("Escuta falhou...\n"); 
 	exit(EXIT_FAILURE); 
@@ -87,10 +116,14 @@ int main()
   else
 	printf("Servidor escutando...\n"); 
 
-  len = sizeof(cli); 
+  tamanho = sizeof(cli); 
 
-  // Aceita a conexão do cliente e verifica.
-  connfd = accept(sockfd, (SA*)&cli, &len); 
+  // Aceita a conexão do cliente.
+  // A primitiva accept recebe tres parametros
+  //   - o descritor do socket
+  //   - a estrutura que tera os dados do cliente conectado
+  //   - o tamanho da estrutura que tera os dados do cliente conectado
+  connfd = accept(sockfd, (SA*)&cli, &tamanho); 
   if (connfd < 0) { 
   	printf("O aceite do servidor falhou...\n"); 
 	exit(EXIT_FAILURE); 
